4e7166f2a7bb1649047a55621ca0d214692b7e0d
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-07-27 01:40:22 -0400

    Created a more complex Guice test

---
+++ tests/dependencyinjection/MissingImplementation.java
@@ -0,0 +1,96 @@
+package dependencyinjection;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Provides;
+import java.lang.annotation.Retention;
+import javax.inject.Inject;
+import javax.inject.Qualifier;
+
+import org.checkerframework.common.value.qual.*;
+
+public class MissingImplementation {
+  @Qualifier
+  @Retention(RUNTIME)
+  @interface Message {}
+
+  @Qualifier
+  @Retention(RUNTIME)
+  @interface Count {}
+
+  /**
+   * Guice module that provides bindings for message and count used in
+   * {@link Greeter}.
+   */
+  static class DemoModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      bind(Baz.class).to(BazImpl.class);
+    }
+
+    @Provides
+    @Count
+    static Integer provideCount() {
+      return 3;
+    }
+
+    @Provides
+    @Message
+    static String provideMessage() {
+      return "hello world";
+    }
+  }
+
+  static class Greeter {
+    private final String message;
+    private final int count;
+
+    // Greeter declares that it needs a string message and an integer
+    // representing the number of time the message to be printed.
+    // The @Inject annotation marks this constructor as eligible to be used by
+    // Guice.
+
+    Greeter(@Message String message, @Count int count) {
+      this.message = message;
+      this.count = count;
+    }
+
+    void sayHello() {
+      for (int i=0; i < count; i++) {
+        System.out.println(message);
+      }
+    }
+
+    String getMessage() {
+      return message;
+    }
+  }
+
+  interface Baz {
+    public void foo();
+  }
+
+  class BazImpl implements Baz {
+    public void foo() {
+      System.out.println("GuiceDemo.BazImpl.foo()");
+    }
+  }
+
+  public static void main(String args[]) {
+    /*
+     * Guice.createInjector() takes one or more modules, and returns a new Injector
+     * instance. Most applications will call this method exactly once, in their
+     * main() method.
+     */
+    Injector injector = Guice.createInjector(new DemoModule());
+    // :: error: MISSING_IMPLEMENTATION
+    Baz baz = injector.getInstance(BazImpl.class);
+
+    // :: error: MISSING_IMPLEMENTATION
+    Greeter greeter = injector.getInstance(Greeter.class);
+
+  }
+}
