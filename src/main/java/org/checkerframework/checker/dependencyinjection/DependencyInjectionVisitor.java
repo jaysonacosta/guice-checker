df376819682a7a43d3858e6adfa1107248e2f471
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-07-27 01:18:51 -0400

    Changed checker to a ClassValChecker. Determining if a class has been bound to another class.

--- src/main/java/org/checkerframework/checker/dependencyinjection/DependencyInjectionVisitor.java
+++ src/main/java/org/checkerframework/checker/dependencyinjection/DependencyInjectionVisitor.java
@@ -1,9 +1,9 @@
 package org.checkerframework.checker.dependencyinjection;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
-import org.checkerframework.common.value.ValueVisitor;
+import org.checkerframework.common.reflection.ClassValVisitor;
 
-public class DependencyInjectionVisitor extends ValueVisitor {
+public class DependencyInjectionVisitor extends ClassValVisitor {
 
   public DependencyInjectionVisitor(BaseTypeChecker c) {
     super(c);
