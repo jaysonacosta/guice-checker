db32dae1d58aee5653917ca076d59ade125a15b3
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-15 09:41:11 -0400

    Accumulating class names during bind method invocations

--- src/main/java/org/checkerframework/checker/dependencyinjection/DependencyInjectionTransfer.java
+++ src/main/java/org/checkerframework/checker/dependencyinjection/DependencyInjectionTransfer.java
@@ -2,7 +2,6 @@ package org.checkerframework.checker.dependencyinjection;
 
 import java.util.List;
 import org.checkerframework.common.accumulation.AccumulationTransfer;
-import org.checkerframework.common.reflection.ClassValAnnotatedTypeFactory;
 import org.checkerframework.common.reflection.ClassValChecker;
 import org.checkerframework.dataflow.analysis.TransferInput;
 import org.checkerframework.dataflow.analysis.TransferResult;
@@ -17,12 +16,10 @@ import org.checkerframework.javacutil.AnnotationUtils;
 public class DependencyInjectionTransfer extends AccumulationTransfer {
 
   private final DependencyInjectionAnnotatedTypeFactory diATF;
-  private final ClassValAnnotatedTypeFactory classValATF;
 
   public DependencyInjectionTransfer(CFAnalysis analysis) {
     super(analysis);
     this.diATF = (DependencyInjectionAnnotatedTypeFactory) analysis.getTypeFactory();
-    this.classValATF = diATF.getTypeFactoryOfSubchecker(ClassValChecker.class);
   }
 
   @Override
@@ -35,13 +32,15 @@ public class DependencyInjectionTransfer extends AccumulationTransfer {
       Node boundClass = node.getArgument(0);
 
       AnnotatedTypeMirror boundClassTypeMirror =
-          this.classValATF.getAnnotatedType(boundClass.getTree());
+          this.diATF
+              .getTypeFactoryOfSubchecker(ClassValChecker.class)
+              .getAnnotatedType(boundClass.getTree());
 
       List<String> classNames =
           AnnotationUtils.getElementValueArray(
               boundClassTypeMirror.getAnnotation(), diATF.classValValueElement, String.class);
 
-      classNames.forEach(name -> accumulate(node, result, name));
+      accumulate(node, result, classNames.toArray(new String[1]));
     }
 
     return result;
