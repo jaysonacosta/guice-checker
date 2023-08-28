babf8ad6adcfa02438064c8b97a86b3c28a69336
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-28 18:19:08 -0400

    Annotate nodes that are calls to Guice's annotatedWith method

--- src/main/java/org/checkerframework/checker/dependencyinjection/DependencyInjectionTransfer.java
+++ src/main/java/org/checkerframework/checker/dependencyinjection/DependencyInjectionTransfer.java
@@ -49,9 +49,6 @@ public class DependencyInjectionTransfer extends AccumulationTransfer {
 
       accumulate(node, result, classNames.toArray(new String[1]));
     } else if (diATF.isAnnotatedWithMethod(node.getTree())) {
-      System.out.println("\u001B[34mDependencyInjectionTransfer.visitMethodInvocation()\u001B[0m");
-      AnnotatedTypeMirror t = diATF.getAnnotatedType(node.getTree());
-
       MethodInvocationNode methodInvocationNode = node;
       MethodInvocationNode callToBindNode =
           (MethodInvocationNode) methodInvocationNode.getTarget().getReceiver();
@@ -60,7 +57,8 @@ public class DependencyInjectionTransfer extends AccumulationTransfer {
       FieldAccessNode boundClass = (FieldAccessNode) callToBindNode.getArgument(0);
       StringLiteralNode givenName = (StringLiteralNode) namesClassNode.getArgument(0);
 
-      accumulateBindAnnotatedWith(node, result, boundClass.toString(), givenName.getValue());
+      accumulateBindAnnotatedWith(
+          node, result, boundClass.getReceiver().getType().toString(), givenName.getValue());
     }
 
     return result;
@@ -73,36 +71,21 @@ public class DependencyInjectionTransfer extends AccumulationTransfer {
     // If dataflow has already recorded information about the target, fetch it and integrate
     // it into the list of values in the new annotation.
     JavaExpression target = JavaExpression.fromNode(node);
-    // TODO: Not sure if this is necessary.
-    // if (CFAbstractStore.canInsertJavaExpression(target)) {
-    //   CFValue flowValue = result.getRegularStore().getValue(target);
-    //   System.out.println("flowValue: " + flowValue);
-    //   if (flowValue != null) {
-    //     AnnotationMirrorSet flowAnnos = flowValue.getAnnotations();
-    //     assert flowAnnos.size() <= 1;
-    //     for (AnnotationMirror anno : flowAnnos) {
-    //       if (diATF.isAccumulatorAnnotation(anno)) {
-    //         List<String> oldFlowValues = diATF.getAccumulatedValues(anno);
-    //         if (!oldFlowValues.isEmpty()) {
-    //           // valuesAsList cannot have its length changed -- it is backed by an
-    //           // array -- but if oldFlowValues is not empty, it is a new, modifiable
-    //           // list.
-    //           oldFlowValues.addAll(valuesAsList);
-    //           valuesAsList = oldFlowValues;
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
 
     AnnotationMirror newAnno = diATF.createBindAnnotatedWithAnnotation(valuesAsList, namesAsList);
-    insertIntoStores(result, target, newAnno);
-    System.out.printf("newAnno: %s\n", newAnno);
+
+    // TODO: insertValue not updating stores, insertValuePermitNondeterministic is
+    if (result.containsTwoStores()) {
+      result.getThenStore().insertValuePermitNondeterministic(target, newAnno);
+      result.getElseStore().insertValuePermitNondeterministic(target, newAnno);
+    } else {
+      result.getRegularStore().insertValuePermitNondeterministic(target, newAnno);
+    }
+
     Tree tree = node.getTree();
 
     if (tree != null && tree.getKind() == Tree.Kind.METHOD_INVOCATION) {
       Node receiver = ((MethodInvocationNode) node).getTarget().getReceiver();
-      System.out.println("receiver: " + receiver);
       if (receiver != null && diATF.returnsThis((MethodInvocationTree) tree)) {
         accumulateBindAnnotatedWith(receiver, result, value, name);
       }
