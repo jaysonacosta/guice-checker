aa760a5924a753ebaec5c6ccfb77187731c68f6a
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-08-29 13:24:26 -0400

    Update javadoc for Bind and BAW

--- src/main/java/org/checkerframework/checker/dependencyinjection/qual/Bind.java
+++ src/main/java/org/checkerframework/checker/dependencyinjection/qual/Bind.java
@@ -8,8 +8,8 @@ import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * If an expression has type {@code @Bind({"foo"})}, then {@code foo.class} has been passed as an
- * argument to {@code bind} in a Guice module.
+ * Represents classes and names that have been passed as arguments to {@link
+ * com.google.inject.Binder#bind()}.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
@@ -17,13 +17,11 @@ import org.checkerframework.framework.qual.SubtypeOf;
 @DefaultQualifierInHierarchy
 public @interface Bind {
   /**
-   * Classes that have definitely been passed as an arugment to a {@code bind} call whose result is
-   * the expression whose type is annotated. The arguments are "fully qualified binary names"
-   * ({@link org.checkerframework.checker.signature.qual.FqBinaryName}): a primitive or <a
-   * href="https://docs.oracle.com/javase/specs/jls/se17/html/jls-13.html#jls-13.1">binary name</a>,
-   * possibly followed by some number of array brackets.
+   * Class names that have definitely been passed as an argument to a {@link
+   * com.google.inject.Binder#bind()} call.
    *
-   * @return classes that have definitely been passed as an arugment to {@code bind}
+   * @return classes that have definitely been passed as an arugment to {@link
+   *     com.google.inject.Binder#bind()}
    */
   public String[] value() default {};
 }
--- src/main/java/org/checkerframework/checker/dependencyinjection/qual/BindAnnotatedWith.java
+++ src/main/java/org/checkerframework/checker/dependencyinjection/qual/BindAnnotatedWith.java
@@ -7,28 +7,33 @@ import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * If an expression has type {@code @Bind({"foo"}, {"bar"})}, then {@code foo.class} has been passed
- * as an argument to {@code bind} and {@code Names.named("bar")} hsa been passed to {@code
- * annotatedWith} in a Guice module.
+ * Represents classes and names that have been passed as arguments to {@link
+ * com.google.inject.Binder#bind()} and {@link
+ * com.google.inject.binder.AnnotatedBindingBuilder#annotatedWith()} respectively.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @SubtypeOf({Bind.class})
 public @interface BindAnnotatedWith {
-  /**
-   * Classes that have definitely been passed as an arugment to a {@code bind} call whose result is
-   * the expression whose type is annotated. The arguments are "fully qualified binary names"
-   * ({@link org.checkerframework.checker.signature.qual.FqBinaryName}): a primitive or <a
-   * href="https://docs.oracle.com/javase/specs/jls/se17/html/jls-13.html#jls-13.1">binary name</a>,
-   * possibly followed by some number of array brackets.
-   *
-   * @return classes that have definitely been passed as an arugment to {@code bind} and names that
-   *     have definitely been passed as an argument to {@code annotatedWith}
-   */
+  /** The fully-qualified name of this annotation. */
   static final String NAME =
       "org.checkerframework.checker.dependencyinjection.qual.BindAnnotatedWith";
 
+  /**
+   * Class names that have definitely been passed as an argument to a {@link
+   * com.google.inject.Binder#bind()} call.
+   *
+   * @return classes that have definitely been passed as an arugment to {@link
+   *     com.google.inject.Binder#bind()}
+   */
   public String[] value() default {};
 
+  /**
+   * Names that have been passed as an argument to an {@link
+   * com.google.inject.binder.AnnotatedBindingBuilder#annotatedWith()} call.
+   *
+   * @return names that have been passed as an argument to {@link
+   *     com.google.inject.binder.AnnotatedBindingBuilder#annotatedWith()}
+   */
   public String[] annotatedWith() default {};
 }
