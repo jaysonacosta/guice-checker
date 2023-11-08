e071d8e22376443da889eb110fb69f6549631f12
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-11-07 22:38:24 -0500

    Create field injection test

--- src/main/java/org/checkerframework/checker/dependencyinjection/DependencyInjectionVisitor.java
+++ src/main/java/org/checkerframework/checker/dependencyinjection/DependencyInjectionVisitor.java
@@ -1,7 +1,11 @@
 package org.checkerframework.checker.dependencyinjection;
 
+import com.sun.source.tree.MethodTree;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.ExecutableElement;
 import org.checkerframework.common.accumulation.AccumulationVisitor;
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.javacutil.TreeUtils;
 
 public class DependencyInjectionVisitor extends AccumulationVisitor {
 
@@ -12,5 +16,17 @@ public class DependencyInjectionVisitor extends AccumulationVisitor {
   @Override
   protected DependencyInjectionAnnotatedTypeFactory createTypeFactory() {
     return new DependencyInjectionAnnotatedTypeFactory(this.checker);
+  }
+
+  @Override
+  public Void visitMethod(MethodTree tree, Void p) {
+    ExecutableElement element = TreeUtils.elementFromDeclaration(tree);
+    AnnotationMirror annotation =
+        this.getTypeFactory().getDeclAnnotation(element, com.google.inject.Inject.class);
+
+    if (annotation != null) {
+      DependencyInjectionAnnotatedTypeFactory.addDependency(annotation.toString(), tree);
+    }
+    return super.visitMethod(tree, p);
   }
 }
