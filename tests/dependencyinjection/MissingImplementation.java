34a18b3187d9077dfecdf8f41aa26718438fab39
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-11-09 21:54:44 -0500

    Fix incorrect annotation import

--- tests/dependencyinjection/MissingImplementation.java
+++ tests/dependencyinjection/MissingImplementation.java
@@ -2,10 +2,10 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Provides;
 import java.lang.annotation.Retention;
-import javax.inject.Inject;
 import javax.inject.Qualifier;
 import org.checkerframework.common.value.qual.*;
 
